c76013a34a09fc44279ba9ebf05724294776b6f6
Kurogoma939 <kan.sss.939@gmail.com>
2023-01-17 20:20:22 +0900

    データ投入

--- lib/infrastructure/data_source/sample/sample_remote_data_source.dart
+++ lib/infrastructure/data_source/sample/sample_remote_data_source.dart
@@ -25,6 +25,66 @@ class SampleDataSource {
         id: '5',
         name: 'サンプルラベル5',
       ),
+      const SampleModel(
+        id: '6',
+        name: 'サンプルラベル6',
+      ),
+      const SampleModel(
+        id: '7',
+        name: 'サンプルラベル7',
+      ),
+      const SampleModel(
+        id: '8',
+        name: 'サンプルラベル8',
+      ),
+      const SampleModel(
+        id: '9',
+        name: 'サンプルラベル9',
+      ),
+      const SampleModel(
+        id: '10',
+        name: 'サンプルラベル10',
+      ),
+      const SampleModel(
+        id: '11',
+        name: 'サンプルラベル11',
+      ),
+      const SampleModel(
+        id: '12',
+        name: 'サンプルラベル12',
+      ),
+      const SampleModel(
+        id: '13',
+        name: 'サンプルラベル13',
+      ),
+      const SampleModel(
+        id: '14',
+        name: 'サンプルラベル14',
+      ),
+      const SampleModel(
+        id: '15',
+        name: 'サンプルラベル15',
+      ),
+      const SampleModel(
+        id: '16',
+        name: 'サンプルラベル16',
+      ),
+      const SampleModel(
+        id: '17',
+        name: 'サンプルラベル17',
+      ),
+      const SampleModel(
+        id: '18',
+        name: 'サンプルラベル18',
+      ),
+      const SampleModel(
+        id: '19',
+        name: 'サンプルラベル19',
+      ),
+      const SampleModel(
+        id: '20',
+        name: 'サンプルラベル20',
+      ),
     ];
   }
 }
