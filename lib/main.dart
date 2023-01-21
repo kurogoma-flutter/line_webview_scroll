885dc1a257d8446483e459ef93eb5092a5c7a979
Kurogoma939 <kan.sss.939@gmail.com>
2023-01-21 22:55:22 +0900

    hosting用Script修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -6,6 +7,10 @@ import 'initialize.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Firebase初期化
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   // アプリ実行前の初期化処理
   await Initialize().init();
   // アプリ実行
